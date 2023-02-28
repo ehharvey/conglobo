65c4af08d21a1407f913dffc140f9abc24cbe1cb
Gavin Abeele <gabeele2160@conestogac.on.ca>
2023-02-28 21:50:17 +0000

    Added API calls for the service.json file

--- api/api.py
+++ api/api.py
@@ -1,65 +1,66 @@
 import json
-from flask import Flask, jsonify, request, send_file 
+from flask import Flask, abort, jsonify, request, send_file 
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello World!"
+with open('services.json', 'r') as f:
+    services = json.load(f)
+    
+with open('active-services.json', 'r') as f:
+    activeServices = json.load(f)
 
+# ------ Single Node API ------ 
 @app.route("/health-check")
 def health_check():
     return {
             "health":"Good", "status":True}
     
-@app.route("/services", methods = ['GET', 'POST'])
-def active_services():
-    if request.method == 'GET':
-        with open('api/services.json', 'r') as f:
-            data = json.load(f)
-            return jsonify(data)
-        
-    elif request.method == 'POST':
+@app.route('/active-services', methods=['GET'])
+def get_services():
+    return jsonify("/api/active-services.json")
 
-        on_services = request.form.get('on')
-        off_services = request.form.get('off')
-        if on_services or off_services:
-            with open('api/services.json', 'r+') as f:
-                data = json.load(f)
-                services = data['services']
-                if on_services:
-                    on_services_list = [name.strip() for name in on_services.split(',')]
-                    for name in on_services_list:
-                        if name in services:
-                            services[name]['active'] = True
-                    on_message = f'Successfully turned on services: {", ".join(on_services_list)}.'
-                else:
-                    on_message = None
-                if off_services:
-                    off_services_list = [name.strip() for name in off_services.split(',')]
-                    for name in off_services_list:
-                        if name in services:
-                            services[name]['active'] = False
-                    off_message = f'Successfully turned off services: {", ".join(off_services_list)}.'
-                else:
-                    off_message = None
-                f.seek(0)
-                json.dump(data, f)
-                f.truncate()
-            messages = []
-            if on_message:
-                messages.append(on_message)
-            if off_message:
-                messages.append(off_message)
-            if messages:
-                message = ' '.join(messages)
-            else:
-                message = 'No services turned on or off.'
-            return jsonify({'message': message})
-        else:
-            return jsonify({'error': 'Missing on or off parameter.'})
+# CRUD services
 
+# ------ Cluster Node API ------ 
 
+# ------ Misc. API ------ 
+
+# Gets all the avaliable services in the services file
+@app.route('/services', methods=['GET'])
+def get_services():
+    return jsonify("/api/services.json")
+
+# Addds a new service to the services file
+@app.route('/services', methods=['POST'])
+def add_service():
+    if not request.json or 'title' not in request.json:
+        abort(400) 
+
+    new_service = {
+        'title': request.json['title'],
+        'description': request.json.get('description', ''),
+    }
+
+    services.append(new_service)
+
+    with open('services.json', 'w') as f:
+        json.dump(services, f)
+
+    return jsonify(new_service), 201  
+
+# Removes a service from the services file
+@app.route('/services/<string:title>', methods=['DELETE'])
+def delete_service(title):
+    for i, service in enumerate(services):
+        if service['title'] == title:
+            services.pop(i)
+
+            with open('services.json', 'w') as f:
+                json.dump(services, f)
+
+            return '', 204  
+
+    abort(404) 
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
