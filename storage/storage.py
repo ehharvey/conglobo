2523050ceb65f66f6aa929c4efbe8c42b2acd873
Gavin Abeele <gabeele2160@conestogac.on.ca>
2023-03-05 18:09:11 +0000

    added services to the bucket. made python script.

--- storage/storage.py
+++ storage/storage.py
@@ -1,31 +1,33 @@
-import os
+from xmlrpc.client import ResponseError
 from minio import Minio
-from minio.error import ResponseError
+from minio.error import S3Error
 
-# Instantiate a MinIO client object with endpoint, access key, and secret key
+# Set up the Minio client
 client = Minio(
-    "localhost:9000",
-    access_key="your-access-key",
-    secret_key="your-secret-key",
+    endpoint = '127.0.0.1:9000',
+    access_key='OCH9CnYNuKFIl47v',
+    secret_key='c0ceU65IyP59YaCgCF4aZqVYaXzTjnM2',
     secure=False
 )
 
-# Set the bucket name
-bucket_name = "your-bucket-name"
-
-# Create the bucket if it doesn't exist
-if not client.bucket_exists(bucket_name):
-    client.make_bucket(bucket_name)
-
-# Set the path to the file to upload
-file_path = "/path/to/your/file.txt"
-
-# Set the object name (the name under which the file will be stored in the bucket)
-object_name = os.path.basename(file_path)
-
+# Create a bucket
 try:
-    # Upload the file to the bucket
-    client.fput_object(bucket_name, object_name, file_path)
-    print(f"{file_path} uploaded successfully!")
+    client.make_bucket('services')
 except ResponseError as err:
-    print(f"Error uploading {file_path}: {err}")
+    print(err)
+
+# Upload a file to the 'services' bucket
+try:
+    # Set the file path and name
+    file_path = 'services.json'
+    file_name = 'services.json'
+
+    # Set the bucket name
+    bucket_name = 'services'
+
+    # Upload the file to the bucket
+    client.fput_object(bucket_name, file_name, file_path)
+
+    print(f'File {file_name} uploaded successfully to bucket {bucket_name}.')
+except S3Error as e:
+    print(f'Error uploading file: {e}')
