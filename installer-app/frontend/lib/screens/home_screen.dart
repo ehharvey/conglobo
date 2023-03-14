bbb69847ca28e65d44a1b424ee5f624a3fe5706f
SanjaySekarSamuel <ssamuel3@conestogac.on.ca>
2023-03-13 23:54:50 -0400

    added check to see for ubuntu installer and writing the auto-install

--- installer-app/frontend/lib/screens/home_screen.dart
+++ installer-app/frontend/lib/screens/home_screen.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/widgets/app_widgets.dart';
 import 'package:frontend/widgets/drop_down_widget.dart';
+import 'package:path/path.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -19,6 +21,7 @@ class _HomeScreenState extends State<HomeScreen> {
   String? _tailScaleKey;
   double _progressValue = 0.0;
   Timer? _timer;
+  bool isUbuntu = false;
 
   void _startSetup() {
     setState(() {
@@ -50,166 +53,193 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text('Conglobo Installer'),
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 150,
-            padding: const EdgeInsets.all(20),
-            width: double.infinity,
-            child: Card(
-              elevation: 10,
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Choose Ubuntu version',
-                      style: TextStyle(
-                          fontSize: 20.0, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    DropdownWidget(
-                      options: const ['Jammy', '22.04.2'],
-                      onChanged: (selectedOption) {
-                        setState(() {
-                          _selectedUbuntiVersion = selectedOption;
-                        });
-                        print(_selectedUbuntiVersion);
-                      },
-                    ),
-                  ]),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 150,
+              padding: const EdgeInsets.all(20),
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Choose Ubuntu version',
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      DropdownWidget(
+                        options: const ['Jammy', '22.04.2'],
+                        onChanged: (selectedOption) {
+                          setState(() {
+                            _selectedUbuntiVersion = selectedOption;
+                          });
+                          print(_selectedUbuntiVersion);
+                        },
+                      ),
+                    ]),
+              ),
             ),
-          ),
-          Container(
-            height: 300,
-            padding: const EdgeInsets.all(20),
-            width: double.infinity,
-            child: Card(
-              elevation: 10,
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Select your VPN',
-                      style: TextStyle(
-                          fontSize: 20.0, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    DropdownWidget(
-                      options: const ['TailScale', 'NetMaker'],
-                      onChanged: (selectedOption) {
-                        setState(() {
-                          _selectedVpn = selectedOption;
-                          if (_selectedVpn == "NetMaker") {
-                            _isNetMaker = true;
-                          } else {
-                            _isNetMaker = false;
-                          }
-                        });
-                        print(_selectedVpn);
-                      },
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    _isNetMaker
-                        ? Column(
-                            children: [
-                              TextField(
-                                decoration: const InputDecoration(
-                                  labelText:
-                                      'Please enter your NetMaker endpoint',
-                                  border: OutlineInputBorder(),
+            Container(
+              height: 300,
+              padding: const EdgeInsets.all(20),
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Select your VPN',
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      DropdownWidget(
+                        options: const ['TailScale', 'NetMaker'],
+                        onChanged: (selectedOption) {
+                          setState(() {
+                            _selectedVpn = selectedOption;
+                            if (_selectedVpn == "NetMaker") {
+                              _isNetMaker = true;
+                            } else {
+                              _isNetMaker = false;
+                            }
+                          });
+                          print(_selectedVpn);
+                        },
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      _isNetMaker
+                          ? Column(
+                              children: [
+                                TextField(
+                                  decoration: const InputDecoration(
+                                    labelText:
+                                        'Please enter your NetMaker endpoint',
+                                    border: OutlineInputBorder(),
+                                  ),
+                                  onChanged: (value) {
+                                    // Do something with the value entered in the text field
+                                  },
                                 ),
-                                onChanged: (value) {
-                                  // Do something with the value entered in the text field
-                                },
-                              ),
-                              const SizedBox(
-                                height: 20,
-                              ),
-                              TextField(
-                                decoration: const InputDecoration(
-                                  labelText: 'Please enter your NetMaker key',
-                                  border: OutlineInputBorder(),
+                                const SizedBox(
+                                  height: 20,
                                 ),
-                                onChanged: (value) {
-                                  // Do something with the value entered in the text field
-                                },
+                                TextField(
+                                  decoration: const InputDecoration(
+                                    labelText: 'Please enter your NetMaker key',
+                                    border: OutlineInputBorder(),
+                                  ),
+                                  onChanged: (value) {
+                                    // Do something with the value entered in the text field
+                                  },
+                                ),
+                              ],
+                            )
+                          : TextField(
+                              decoration: const InputDecoration(
+                                labelText: 'Please enter your TailScale key',
+                                border: OutlineInputBorder(),
                               ),
-                            ],
-                          )
-                        : TextField(
-                            decoration: const InputDecoration(
-                              labelText: 'Please enter your TailScale key',
-                              border: OutlineInputBorder(),
-                            ),
-                            onChanged: (value) {
-                              // Do something with the value entered in the text field
-                              if (value != '') {
-                                _tailScaleKey = value;
-                              }
-                            },
-                          )
-                  ]),
+                              onChanged: (value) {
+                                // Do something with the value entered in the text field
+                                if (value != '') {
+                                  _tailScaleKey = value;
+                                }
+                              },
+                            )
+                    ]),
+              ),
             ),
-          ),
-          Container(
-            height: 150,
-            padding: const EdgeInsets.all(20),
-            width: double.infinity,
-            child: Card(
-              elevation: 10,
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Insert USB and Select driver',
-                      style: TextStyle(
-                          fontSize: 20.0, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      children: [
-                        ElevatedButton(
-                            onPressed: () async {
-                              final folderPath =
-                                  await FilePicker.platform.getDirectoryPath();
-                              print('Folder path: $folderPath');
-                            },
-                            child: const Text("Select USB driver")),
-                        const SizedBox(
-                          width: 20,
-                        ),
-                        const Text(
-                          'No USB detected',
-                          style: TextStyle(color: Colors.red),
-                        )
-                      ],
-                    ),
-                  ]),
+            Container(
+              height: 150,
+              padding: const EdgeInsets.all(20),
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Insert USB and select the USB folder',
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: [
+                          ElevatedButton(
+                              onPressed: () async {
+                                String? selectedDirectory = await FilePicker
+                                    .platform
+                                    .getDirectoryPath();
+
+                                if (selectedDirectory == null) {
+                                  // User canceled the picker
+                                } else {
+                                  //print(selectedDirectory);
+                                  var path = selectedDirectory;
+                                  final dir = Directory(path);
+                                  final files = await dir.list().toList();
+                                  for (final file in files) {
+                                    final bname = basename(file.path);
+                                    final ext = extension(bname);
+                                    if (bname.startsWith('ubuntu') &&
+                                        ext == '.iso') {
+                                      final file1 = File('$path/user-data.txt');
+                                      final file2 = File('$path/meta-data.txt');
+                                      await file1.writeAsString('Testing');
+                                      print('File created');
+                                    }
+                                  }
+                                }
+                              },
+                              child: const Text("Select USB driver")),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          !isUbuntu
+                              ? const Text(
+                                  'USB folder with ubutu installed not found!',
+                                  style: TextStyle(color: Colors.red),
+                                )
+                              : const Text(
+                                  'Ubuntu installer found',
+                                  style: TextStyle(color: Colors.green),
+                                )
+                        ],
+                      ),
+                    ]),
+              ),
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              _startSetup();
-            },
-            child: const Text("BEGIN INSTALLATION"),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          LinearProgressIndicator(
-            value: _progressValue,
-            backgroundColor: Colors.grey,
-            valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
-          )
-        ],
+            ElevatedButton(
+              onPressed: () {
+                _startSetup();
+              },
+              child: const Text("BEGIN INSTALLATION"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            LinearProgressIndicator(
+              value: _progressValue,
+              backgroundColor: Colors.grey,
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+            )
+          ],
+        ),
       ),
     );
   }
